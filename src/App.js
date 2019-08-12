6cad8afc42ffc7601d1ad18ba10903557c47174d
Yovanny López <ylopezc@misena.edu.co>
2019-08-11 21:12:14 -0500

    test Github API v4 at GraphQL with React and Apollo

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class App extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div className="container">
+                {this.props.data.loading === true ? "Loading" : this.props.data.search.edges.map(data =>
+                    <ul key={data.node.id}>
+                        <li style={{ fontWeight: 'bold' }}>
+                            Repository: <a href={data.node.url}>{data.node.name}</a>
+                        </li>
+                        <li><strong>Description:</strong>   {data.node.description}
+                        </li>
+                        <li><strong>CreatedAt: </strong>{data.node.createdAt}</li>
+                        <li><strong>PushedAt: </strong>{data.node.pushedAt}</li>
+                        <li><strong>Type:</strong> {data.node.isPrivate === true ? "Private" : "Public"}</li>
+                    </ul>
+                )}
+            </div>
+        );
+    }
 }
 
-export default App;
+const repoQuery = gql`
+    query($name: String!){
+        search(query: $name, last: 50, type: REPOSITORY) {
+            edges {
+                node {
+                    ... on Repository {
+                        id
+                        name
+                        description
+                        url
+                        pushedAt
+                        createdAt
+                        isPrivate
+                    }
+                }
+            }
+        }
+    }
+`
+
+const AppWithData = graphql(
+    repoQuery,
+    {
+        options: {
+            variables: {
+                name: "react-graphql"
+            }
+        }
+    }
+)(App)
+
+export default AppWithData;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css'
+
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const token = "5ab4c254fed0c17f0a8402fa20cf73c3a2d4cfae";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const httpLink = {
+    uri: 'https://api.github.com/graphql',
+    headers: {
+        authorization: `Bearer ${token}`
+    }
+};
+
+const client = new ApolloClient({
+    link: new HttpLink(httpLink),
+    cache: new InMemoryCache()
+});
+
+ReactDOM.render(<ApolloProvider client={client}><App /></ApolloProvider>, document.getElementById('root'));
+
 serviceWorker.unregister();
